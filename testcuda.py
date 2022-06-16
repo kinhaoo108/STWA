d19b1ecf6e486848045718699b6db50869fc3bb5
kinhaoo108 <kinhaoo108@163.com>
2022-06-16 12:43:19 +0800

    testcuda.py

---
+++ testcuda.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 15 15:40:13 2021
+
+@author: JiaHao
+"""
+
+import torch
+print(torch.__version__)
+print(torch.cuda.is_available())
+
+#conda install pytorch torchvision cudatoolkit=11.8 -c pytorch
